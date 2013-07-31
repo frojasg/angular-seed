f90cc71be653c3d253e95352d5381002cc97cbba
Francisco Rojas <frojas@groupon.com>
2013-07-31 00:34:59 -0400

    Use restler wrapper to fetch the image in flickr
    Use restler wrapper which add cache in the call to the flickr API

--- test/mocha/lib/clients/flickr.js
+++ test/mocha/lib/clients/flickr.js
@@ -2,6 +2,8 @@ var assert = require('assert');
 var should = require('should');
 var nock = require('nock');
 var __ = require('underscore');
+var MemJS = require("memjs").Client;
+var memjs = MemJS.create();
 
 var Flickr = require('../../../../lib/clients/flickr/flickr.js');
 
@@ -9,14 +11,13 @@ describe('Flickr API client', function(){
   describe('when we call to get a random photo', function(){
     before(function() {
       process.env.flickrApiKey = 'ola';
+      memjs.flush();
     });
-
 
     it('should get a list of size of one image', function(done){
       nock('http://api.flickr.com/')
         .get('/services/rest/?nojsoncallback=1&format=json&api_key=ola&photoset_id=72157634695438319&method=flickr.photosets.getPhotos')
         .replyWithFile(200, __dirname + '/data/flickr/photoset.json');
-
 
       nock('http://api.flickr.com/')
         .filteringPath(/photo_id=[^&]*/g, 'photo_id=9319145856')
@@ -57,5 +58,39 @@ describe('Flickr API client', function(){
       });
     });
 
+    it('should', function(done) {
+      nock('http://api.flickr.com/')
+        .get('/services/rest/?nojsoncallback=1&format=json&api_key=ola&photoset_id=72157634695438319&method=flickr.photosets.getPhotos')
+        .replyWithFile(200, __dirname + '/data/flickr/photoset.json');
+
+      nock('http://api.flickr.com/')
+        .filteringPath(/photo_id=[^&]*/g, 'photo_id=9319145856')
+        .get('/services/rest/?nojsoncallback=1&format=json&api_key=ola&method=flickr.photos.getSizes&photo_id=9319145856')
+        .replyWithFile(200, __dirname + '/data/flickr/photo_id_9319145856.json');
+
+        var flickr = new Flickr();
+      Flickr.should.be.a('function');
+      flickr.should.be.a('object');
+
+      flickr.getRandomImage(function (photos) {
+        photos.should.be.a('object');
+        __.find(photos, function(photo) { return photo.label == 'Small 320'; }).should.have.property('source');
+        __.find(photos, function(photo) { return photo.label == 'Large'; }).should.have.property('source');
+        __.each(photos, function(photo) {
+          photo.should.have.property('source');
+        });
+      });
+
+      flickr.getRandomImage(function (photos) {
+        photos.should.be.a('object');
+        __.find(photos, function(photo) { return photo.label == 'Small 320'; }).should.have.property('source');
+        __.find(photos, function(photo) { return photo.label == 'Large'; }).should.have.property('source');
+        __.each(photos, function(photo) {
+          photo.should.have.property('source');
+        });
+
+        done();
+      });
+    });
   });
 });
