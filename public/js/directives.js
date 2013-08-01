8fe802e5100da7afd12fc5864a858943aca212fc
Francisco Rojas <frojas@groupon.com>
2013-07-31 23:23:24 -0400

    Add background color meanwhile the background image is load

--- public/js/directives.js
+++ public/js/directives.js
@@ -11,17 +11,24 @@ angular.module('myApp.directives', []).
   }]).directive('backImg', function(){
       return function(scope, element, attrs){
         scope.$watch('background', function(){
-          element.css({
-            'background-image': 'url(' + scope.background +') ',
-            'background-size' : 'cover',
-            '-webkit-background-size' : 'cover',
-            '-moz-background-size': 'cover',
-            '-o-background-size': 'cover',
-            'background-repeat': 'no-repeat',
-            'background-attachment': 'fixed',
-            'background-position': 'center',
-            'background-center': 'center'
-          });
+          if (scope.background){
+            element.css({
+              'background-image': 'url(' + scope.background +') ',
+              'background-size' : 'cover',
+              '-webkit-background-size' : 'cover',
+              '-moz-background-size': 'cover',
+              '-o-background-size': 'cover',
+              'background-repeat': 'no-repeat',
+              'background-attachment': 'fixed',
+              'background-position': 'center',
+              'background-center': 'center'
+            });
+          }else {
+            element.css({
+              'background-color': '#FAFBFC',
+              'background': 'rgba(0, 0, 0, 0.3)'
+            });
+          }
         });
 
       };
